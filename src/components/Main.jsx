1f8a342f0b65abb6243047eb11b50e2a02dcdb33
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-03-09 19:04:30 +0100

    end exercise

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,12 @@ import { useState } from "react"
 import { useApiContext } from "../contexts/ApiContexts"
 const Main = () => {
     const { films, flagSet, Star, tvSeries } = useApiContext()
+    //NOTE - State per tenere traccia della card 
     const [hoveredCard, setHoveredCard] = useState(null)
     return (
         <main>
             <div className="container">
-                <p>Film</p>
+            <section><p>Film</p></section>
                 <div className="Film cards">
                     {/* map attraverso l'array `films` per ottenere i  dettagli di ogni film ricercato */}
                     {
@@ -16,9 +17,12 @@ const Main = () => {
                             films.map((film) => {
                                 const { id, title, original_title, original_language, vote_average, poster_path, overview } = film;
                                 return (
-                                    <div className="Card">
+                                    //NOTE -  Imposta l'ID della card hoverata
+                                    //NOTE - Resetta quando il mouse esce
+                                    <div key={id} onMouseEnter={() => setHoveredCard(id)} onMouseLeave={() => setHoveredCard(null)} className="Card">
                                         <div className="relative">
                                             <img className="Poster" src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="" />
+                                            {hoveredCard === id && ( // Mostra il div solo se la card è hoverata
                                             <div className="absolute">
                                                 <ul key={id}>
                                                     <li>Titolo</li>
@@ -30,14 +34,12 @@ const Main = () => {
                                                     <li >{vote_average}</li>
                                                     <li>Stelle</li>
                                                     <li className="yellow">Voto {Star(vote_average)}</li>
+                                                    <li>Trama</li>
                                                     <li>{overview}</li>
                                                 </ul>
                                             </div>
+                                             )}
                                         </div>
-
-
-
-
                                     </div>
                                 );
                             }
@@ -45,7 +47,7 @@ const Main = () => {
                         )
                     }
                 </div>
-                <p>Serie TV</p>
+                <section><p>Serie Tv</p></section>
                 <div className="SerieTv cards">
                     {
                         tvSeries.length === 0 ? (
@@ -54,11 +56,15 @@ const Main = () => {
                             tvSeries.map((tvSerie) => {
                                 const { id, name, original_name, origin_country, vote_average, poster_path, overview } = tvSerie;
                                 return (
-                                    <div key={id} className="Card">
+                                    
+                                    //NOTE -  Imposta l'ID della card hoverata
+                                    //NOTE - Resetta quando il mouse esce
+                                    <div key={id} onMouseEnter={() => setHoveredCard(id)} onMouseLeave={() => setHoveredCard(null)} className="Card">
                                         <div className="relative">
                                             <img className="Poster" src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="" />
-                                            <div className="absolute">
-                                                <ul>
+                                            {hoveredCard === id && ( // Mostra il div solo se la card è hoverata
+                                                <div className="absolute">
+                                                    <ul>
                                                     <li>Titolo</li>
                                                     <li>{name}</li>
                                                     <li>Titolo Originale</li>
@@ -67,11 +73,12 @@ const Main = () => {
                                                     <li>Voto</li>
                                                     <li> {vote_average}</li>
                                                     <li>Stelle</li>
-                                                    <li>Trama</li>
                                                     <li className="yellow">{Star(vote_average)}</li>
+                                                    <li>Trama</li>
                                                     <li>{overview}</li>
                                                 </ul>
                                             </div>
+                                            )}
                                         </div>
                                     </div>
                                 )
